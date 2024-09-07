1d619b617834154909052f68f7d925664251fd26
Humberto12-Xaxy <humbertosuriano24@gmail.com>
2024-09-06 23:25:47 -0600

    add router

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ import flet
 from flet import *
 
 from database.create_table import create_table
-from views.main_view import MainView
-
+from router import Router
 create_table()
 
 def main(page: Page):
-    MainView(page)
+    page.title = "Punto de Venta"
+
+    router = Router(page)
+
+    def on_router_change(route):
+        route = page.route
+        router.handle_route_change(route)
+    
+    page.on_route_change = on_router_change
+    page.go('/')
 
 
 flet.app(target=main)
--- views/main_view.py
+++
@@ -1,8 +0,0 @@
-import flet as ft
-from flet import Text
-
-class MainView:
-    def __init__(self, page: ft.Page):
-        self.page = page
-        self.page.title = "Punto de Venta"
-        self.page.add(Text("Hello, Flet!"))
